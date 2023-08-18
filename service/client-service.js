638620c9548bcaf25f11c26e33513392fa6ce400
jonhatan <A01769156@tec.mx>
2023-08-18 16:05:17 -0600

    clase 05 y modulo 5 completo

--- service/client-service.js
+++ service/client-service.js
@@ -26,11 +26,31 @@ const elimincarCliente = (id) => {
   return fetch(`http://localhost:3000/perfil/${id}`, {
     method: "DELETE",
 
+  });
+};
+
+const detalleCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`).then( (respuesta) => 
+    respuesta.json()
+  );
+};
+
+const acualizarCliente = (nombre, email, id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`,{
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({nombre, email})
   })
-}
+    .then( respuesta => respuesta)
+    .catch((err) => console.log(err));
+};
 
 export const clientServices = {
   listaClientes, 
   crearCliente,
   elimincarCliente,
+  detalleCliente,
+  acualizarCliente,
 };
